f0e218c30f8012e650bd80698dacffa6dc3cba1e
meehdii18 <mehdiaml@icloud.com>
2024-11-08 12:18:58 +0100

    Update auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFirebase } from './FirebaseContext.jsx';
 import appleLogo from './assets/apple.svg';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import './App.css';
 
 function App() {
     const { db, auth } = useFirebase();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            console.log('User signed in:', userCredential.user);
+        } catch (error) {
+            console.error('Error signing in:', error);
+        }
+    };
 
     return (
         <>
@@ -18,7 +30,19 @@ function App() {
             <div className="connexionDiv">
                 <p className="welcome">Bienvenue!</p>
                 <p className="pleaseid">Veuillez vous identifiez</p>
-                <button>Connexion</button>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Mot de passe"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={handleLogin}>Connexion</button>
             </div>
         </>
     );
