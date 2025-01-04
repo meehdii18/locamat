0aa70850a98a4000b8de881ab7cf145143242c46
homerduc <alexandre.duc00@orange.fr>
2025-01-04 19:30:35 +0100

    Added createuser and userpage css details

--- src/pages/Admin/CreateUser/CreateUser.jsx
+++ src/pages/Admin/CreateUser/CreateUser.jsx
@@ -19,25 +19,22 @@ const CreateUser = () => {
     const validateForm = () => {
         let newErrors = {};
 
-        // Email Validation
         if (!/^.+@locamat\.fr$/.test(formData.email)) {
             newErrors.email = "Email must be of type xxx@locamat.fr";
         }
 
-        // Name and LastName Validation
         if (!/^[a-zA-Z0-9]{1,30}$/.test(formData.firstName)) {
             newErrors.firstName = "First name must be alphanumeric and between 1-30 characters";
         }
+
         if (!/^[a-zA-Z0-9]{1,30}$/.test(formData.lastName)) {
             newErrors.lastName = "Last name must be alphanumeric and between 1-30 characters";
         }
 
-        // Phone number Validation
         if (!/^\d{10}$/.test(formData.phoneNumber)) {
             newErrors.phoneNumber = "Phone number must be exactly 10 digits";
         }
 
-        // Password Validation
         if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/.test(formData.password)) {
             newErrors.password = "Password must contain at least 1 lowercase, 1 uppercase, 1 number, 1 special character and be 8+ characters";
         }
@@ -47,7 +44,7 @@ const CreateUser = () => {
     };
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault(); // Prevent reloading of the page
         if (validateForm()) {
             try {
                 // Firebase Auth Registration
