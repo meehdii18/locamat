075b4055469b44ace68196e8e029c60feac34736
Raphaël Aveline <raphael.aveline@etu.univ-tours.fr>
2025-01-04 18:11:39 +0100

    init admin_users

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Login from './pages/Login/Login.jsx'
 import Hardware from './pages/Hardware/Hardware.jsx'
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx';
-
+import Admin_Users from './pages/Admin/Users/Users.jsx';
+import {Component} from "react";
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
                     <Route path="/" element={<Login/>}/>
                     <Route path="/home" element={<Home/>}/>
                     <Route path="/hardware/:id" element={<Hardware/>}/>
+                    <Route path="/admin/users" element={<Admin_Users/>}/>
                 </Routes>
                 <Footer/>
             </Router>
---
+++ src/pages/Admin/Users/Users.jsx
@@ -0,0 +1,9 @@
+function Admin_Users() {
+    return (
+        <div>
+            <h1>Admin Users</h1>
+        </div>
+    );
+}
+
+export default Admin_Users;
