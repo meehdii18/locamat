65d69f3fcb63cdfc18867317e5f13a062298a76f
Mehdi <mehdiaml@icloud.com>
2025-01-22 12:29:47 +0100

    feat: make unpickable booked dates

--- src/pages/Hardware/Booking.jsx
+++ src/pages/Hardware/Booking.jsx
@@ -6,6 +6,7 @@ import { db, auth } from "../../firebase.js";
 import { doc, setDoc } from "firebase/firestore";
 import PropTypes from "prop-types";
 import { collection, query, where, getDocs } from "firebase/firestore";
+import './Booking.css';
 
 const Booking = ({ onClose, hardwareId, onSuccess }) => {
     const [formData, setFormData] = useState({
@@ -15,6 +16,7 @@ const Booking = ({ onClose, hardwareId, onSuccess }) => {
         user: auth.currentUser.uid,
     });
     const [error, setError] = useState(null);
+    const [bookedDates, setBookedDates] = useState([]);
 
     useEffect(() => {
         if (auth.currentUser) {
@@ -23,7 +25,25 @@ const Booking = ({ onClose, hardwareId, onSuccess }) => {
                 user: auth.currentUser.uid,
             }));
         }
+        fetchBookedDates();
     }, []);
+
+    const fetchBookedDates = async () => {
+        const bookingsRef = collection(db, "booking");
+        const q = query(bookingsRef, where("hardwareId", "==", hardwareId));
+        const querySnapshot = await getDocs(q);
+        const dates = querySnapshot.docs.flatMap(doc => {
+            const data = doc.data();
+            const startDate = data.startDate.toDate();
+            const endDate = data.endDate.toDate();
+            const dateArray = [];
+            for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+                dateArray.push(new Date(d));
+            }
+            return dateArray;
+        });
+        setBookedDates(dates);
+    };
 
     const handleStartDateChange = (date) => {
         setFormData({ ...formData, startDate: date });
@@ -83,6 +103,7 @@ const Booking = ({ onClose, hardwareId, onSuccess }) => {
                 startDate={formData.startDate}
                 endDate={formData.endDate}
                 placeholderText="Select start date"
+                excludeDates={bookedDates}
             />
             <Box mt={2}>
                 <Typography variant="h6" gutterBottom textAlign="center">
@@ -97,6 +118,7 @@ const Booking = ({ onClose, hardwareId, onSuccess }) => {
                     endDate={formData.endDate}
                     minDate={formData.startDate}
                     placeholderText="Select end date"
+                    excludeDates={bookedDates}
                 />
             </Box>
             <Button onClick={onClose} color="secondary" variant="outlined" style={{ marginTop: '20px' }}>
