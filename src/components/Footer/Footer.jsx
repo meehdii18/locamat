fb044daabd236bb541a39092299b7b75a18a7908
Evan Lemonnier <165893283+EvanLem@users.noreply.github.com>
2025-01-26 20:12:05 +0100

    Delete src directory

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css';
-
-export default function Footer() {
-    return (
-        <footer className="footer">
-            <p>&copy; 2025 Locamat. All rights reserved.</p>
-        </footer>
-    );
-}
